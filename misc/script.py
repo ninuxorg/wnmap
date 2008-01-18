0820a551aa820d88790fa3a0dbfc6b2a106e4cf5
donato <devnull@localhost>
2008-01-18 04:22:56 -0800

    [svn] node query bug fixed

--- misc/script.py
+++ misc/script.py
@@ -3,12 +3,29 @@ import os
 import re
 import MySQLdb
 
+# Authors: OrazioPirataDelloSpazio, Don@TuX, Si_Mo
+# This software is released under GPL3
+# Ninux.org 2008
+
+#This script read the the txt file from olsrd_txtinfo plug-in and insert in the wnmap database active wireless links
+
 #Mysql variables
 host = "www.bzzauz.org"
 user="wnmap"
 passwd="wnmap"
 db="wnmapunstable"
 table="links"
+
+#Link quality threshold
+good_link=1.2
+bad_link=2
+good=1
+medium=2
+bad=3
+
+#Wireless Nodes ID
+id_endpoint1=''
+id_endpoint2=''
 
 ########################### IMPLEMENTATION #############################
 
@@ -18,7 +35,7 @@ os.system(" wget http://127.0.0.1:2006 -q -O topology.txt")
 #open file
 topology_file=open("topology.txt",'r')
 parsing=False
-#open
+#open 
 try:
 	conn = MySQLdb.connect(host,user,passwd,db)
 except MySQLdb.Error, e:
@@ -31,12 +48,24 @@ for line in topology_file.readlines():
 		if line.isspace():
 			parsing=False
 			break
-		endpoint1=line.split()[0]
-		endpoint2=line.split()[1]
+		endpoint1=line.split()[0] #IP address of endpoint1
+		endpoint2=line.split()[1] #IP address of endpoint2
 		if endpoint1.find('172.16.') != -1 and endpoint2.find('172.16.') != -1:
+			id_endpoint1=cursore.execute('SELECT `id` FROM `nodes` WHERE `nodeIP`="%s"' % (endpoint1))
+			id_endpoint2=cursore.execute('SELECT `id` FROM `nodes` WHERE `nodeIP`="%s"' % (endpoint2))
 			etx=line.split()[4]
-			mysql_query=mysql_query + "(%s,%s,'wifi',%s)" % (endpoint1,endpoint2,etx)
+			link_quality=''
+			if etx <= good_link:
+				etx = good 
+			elif good_link > etx > bad_link:
+				etx = medium
+			elif etx >= bad_link:
+				etx = bad
+			if mysql_query != '': 
+				mysql_query=mysql_query + ','
+			mysql_query=mysql_query + "(%s,%s,'wifi',%s)" % (id_endpoint1,id_endpoint2,etx)
 	if line.find('Destination IP') != -1:
-		cursore.execute('INSERT INTO `links` (`node1`,`node2`,`type`,`quality`) VALUES '+ mysql_query )
-		parsing=True
-
+		if id_endpoint1 != '' or id_endpoint2 != '' :
+			print ("ESECUZIONE QUERY")
+			cursore.execute('INSERT INTO `links` (`node1`,`node2`,`type`,`quality`) VALUES %s' % mysql_query )
+		parsing=True	
