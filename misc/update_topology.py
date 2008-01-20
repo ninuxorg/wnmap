0cd6e9b0aa0e6976aed461a293e3833babbdbf3f
lorenzo <devnull@localhost>
2008-01-20 15:20:54 -0800

    [svn] update topology: calculate properly average etx and draw only a link

--- misc/update_topology.py
+++ misc/update_topology.py
@@ -2,30 +2,25 @@
 import os
 import re
 import MySQLdb
-from string import atof
-# Authors: OrazioPirataDelloSpazio, Don@TuX, Si_Mo
+
+# Authors: OrazioPirataDelloSpazio, Don@TuX, Si_Mo, Nino
 # This software is released under GPL3
 # Ninux.org 2008
 
 #This script read the the txt file from olsrd_txtinfo plug-in and insert in the wnmap database active wireless links
+#Need python-mysql and wget packages
+
 
 #Mysql variables
-host = "localhost"
+host = "www.bzzauz.org"
 user="wnmap"
-passwd="wnmap_pwd"
+passwd="wnmap"
 db="wnmapunstable"
 table="links"
 
 #Link quality threshold
 good_link=1.2
 bad_link=2
-good=1
-medium=2
-bad=3
-
-#Wireless Nodes ID
-id_endpoint1=''
-id_endpoint2=''
 
 ########################### IMPLEMENTATION #############################
 
@@ -42,7 +37,15 @@ except MySQLdb.Error, e:
 	print "Error %d: %s" % (e.args[0], e.args[1])
 cursore = conn.cursor()
 cursore.execute('DELETE FROM `links` WHERE `links`.`type`="wifi"')
+assert good_link <=  bad_link, "Good link threshold must be lesser equal than bad link threshold !"
 mysql_query=''
+link_quality=''
+links_etx = {}
+#Wireless Nodes ID
+id_endpoint1=''
+id_endpoint2=''
+#quality_values
+quality_values = {'good' : 1, 'medium' : 2, 'bad': 3}
 for line in topology_file.readlines():
 	if parsing:
 		if line.isspace():
@@ -56,38 +59,51 @@ for line in topology_file.readlines():
 			break
 		endpoint1=line.split()[0] #IP address of endpoint1
 		endpoint2=line.split()[1] #IP address of endpoint2
+		#look if there is already a link between these two nodes
 		cursore.execute('SELECT `id` FROM `links` WHERE `node1`="%s" AND `node2`="%s"' % (endpoint1,endpoint2))
-		if cursore.fetchall() == () :
-			cursore.execute('SELECT `id` FROM `nodes` WHERE `nodeIP`="%s"' % (endpoint1))
+		
+		if cursore.rowcount == 0 : # if no links between nodes...(i.e. no VPN links)
+			cursore.execute('SELECT `id` FROM `nodes` WHERE `nodeIP` LIKE "%s"' % (endpoint1))
 			data = cursore.fetchall()
-			if data==():
-				print endpoint1
+			if cursore.rowcount == 0: 
+				print "No nodes with IP: %s in the nodes table" % (endpoint1)
 				continue
-			else:
-				print "OK"
-			print data[0]
 			id_endpoint1=data[0][0]
-			cursore.execute('SELECT `id` FROM `nodes` WHERE `nodeIP`="%s"' % (endpoint2))
-			data2 = cursore.fetchall()
-			if data2==():
-				print endpoint2
+			
+			cursore.execute('SELECT `id` FROM `nodes` WHERE `nodeIP` LIKE "%s"' % (endpoint2))
+			data = cursore.fetchall()
+			if cursore.rowcount == 0: 
+				print "No nodes with IP: %s in the nodes table" % (endpoint2)
 				continue
-			else:
-				print "OK2"
-			print data2[0]
-			id_endpoint2=data2[0][0]
-			etx=line.split()[4]
-			link_quality=''
-			netx=atof(etx)
-			if netx <= good_link:
-				link_quality = good 
-			elif good_link > netx > bad_link:
-				link_quality = medium
-			elif netx >= bad_link:
-				link_quality = bad
-			if mysql_query != '': 
-				mysql_query=mysql_query + ','
-			mysql_query=mysql_query + '(%s,%s,"wifi",%s)' % (id_endpoint1,id_endpoint2,link_quality)
-			print mysql_query
+			id_endpoint2=data[0][0]
+
+			etx=float(line.split()[4])
+
+			if id_endpoint1 + id_endpoint2 not in links_etx:
+				links_etx[id_endpoint2 + id_endpoint1] = etx
+				continue #wait the other monodirectional link...
+			
+			# if found the other link, we can calculate the average etx and prepare the mysql query
+			avg_etx = (links_etx[id_endpoint1 + id_endpoint2] + etx )/ 2
+			del(links_etx[id_endpoint1 + id_endpoint2])
+			
+			print "Created link from node %s to node %s with medium etx %f" % (endpoint1, endpoint2, avg_etx)
+			if avg_etx <= good_link:
+				link_quality = quality_values['good'] 
+				print "good"
+			elif good_link < avg_etx < bad_link:
+				link_quality = quality_values['medium'] 
+				print "medium"
+			elif avg_etx >= bad_link:
+				link_quality = quality_values['bad'] 
+				print "bad"
+			
+			if id_endpoint1 != id_endpoint2:
+				if mysql_query != '': 
+					mysql_query = mysql_query + ','
+				mysql_query = mysql_query + '(%s,%s,"wifi",%s)' % (id_endpoint1,id_endpoint2,link_quality)
+				
+				
 	if line.find('Destination IP') != -1:
 		parsing=True	
+
