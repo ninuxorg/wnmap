d0452dc5f0d02101679b2e4e4967e611bad9da16
Claudio Mignanti <c.mignanti@gmail.com>
2011-02-12 14:33:23 +0100

    manager to enable/disable a node

--- htdocs/manager.php
+++ htdocs/manager.php
@@ -1,3 +1,4 @@
+<?php
 /*
 WNMap
 Copyright (C) 2011 Claudio Mignanti <c.mignanti@gmail.com>
@@ -17,27 +18,32 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-<?php
+
 
 require ("config.php");
 
-
-$name = mysql_real_escape_string ($_GET["name"]);
-if ($hash == '')
-	die ("Error name must be specified!");
-$val = mysql_real_escape_string ($_GET["val"]);
+echo "<h2>Manager</h2>";
 
 $connection = mysql_connect (MYSQL_HOST, MYSQL_USER, MYSQL_PASS) or die ('Could not connect: ' . mysql_error());
 mysql_select_db (MYSQL_DB) or die ('Could not select database.');
 
-/* Status change - manager.php?hash='+hash+'&action=status&val='+new_value */
+$name = mysql_real_escape_string ($_GET["name"]);
+if ($name == '')
+	die ("Error: name must be specified!");
+$val = mysql_real_escape_string ($_GET["val"]);
+
+/* Status change - manager.php?name='+name+'&action=status&val='+new_value */
 if (isset($_GET["action"]) &&  $_GET["action"] == "status" ) {
 
-	$query = "UPDATE nodes SET status=" . $val . " WHERE nodeName=". $name .";";
-	
-	mail ("");
+	$query = "UPDATE nodes SET status=" . $val . " WHERE nodeName='". $name ."';";
+
+	$result = mysql_query ($query, $connection) or die (mysql_error());
+
+	echo "Lo stato del tuo nodo è stato aggiornato correttamente.<br> Ricarica la pagina del mapserver se non dovesse essere aggiornata.";
 
 }
+
+echo "<a href=\"javascript:void(0);\" onclick=\"window.close();\">Chiudi</a>";
 
 /* TODO: Ip Change */
 
